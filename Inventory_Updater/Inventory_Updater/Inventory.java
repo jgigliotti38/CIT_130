07807952354395d9ceccba5bc54121fe03b9368d
jgigliotti38 <43352647+jgigliotti38@users.noreply.github.com>
2018-12-10 15:22:28 -0500

    Add files via upload

---
+++ Inventory_Updater/Inventory_Updater/Inventory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inventory_Updater;
+
+/**
+ *
+ * @author jgigl16
+ */
+import java.util.*;
+public class Inventory {
+    int productNum;
+    String productName;
+    float sellPrice;
+    float unitCost;
+    int inventoryAmount;
+
+
+}
